d65209d6793a94a703eaf3a75072d6e116d3a5a3
panevka <97028404+panevka@users.noreply.github.com>
2024-11-30 21:15:24 +0100

    Draft visual components addition

---
+++ PomodoroApp/Timer.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PomodoroApp;
+
+public partial class Timer : UserControl
+{
+    public Timer()
+    {
+        InitializeComponent();
+    }
+}
