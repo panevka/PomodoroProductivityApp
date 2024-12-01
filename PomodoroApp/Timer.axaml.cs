80728e0b63d7f6ba3588cddf2775efbf95bf9b20
panevka <97028404+panevka@users.noreply.github.com>
2024-12-02 00:43:52 +0100

    Basic countdown implementation for timer

--- PomodoroApp/Timer.axaml.cs
+++ PomodoroApp/Timer.axaml.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
@@ -5,9 +8,34 @@ using Avalonia.Markup.Xaml;
 namespace PomodoroApp;
 
 public partial class Timer : UserControl
-{
+{ 
     public Timer()
     {
         InitializeComponent();
+        StartTimerCountDown(Time, 6080);
+
     }
+
+    private async void StartTimerCountDown(TextBlock timeComponent, int time)
+    {
+        await UpdateTimer(timeComponent, time);
+    }
+    
+    private static async Task UpdateTimer(TextBlock timeComponent, int baseTime)
+    {
+        int seconds, hours, minutes;
+        
+        while (baseTime != 0)
+        {
+            baseTime--;
+            seconds = baseTime % 60;
+            hours = Convert.ToInt32(baseTime / 60 / 60);
+            minutes = (baseTime - baseTime % 60) / 60 - hours * 60;
+            timeComponent.Text = $"{hours.ToString("00")}:{minutes.ToString("00")}:{seconds.ToString("00")}";
+            await Task.Delay(1000);
+        }
+    }
+    
+    
+    
 }
