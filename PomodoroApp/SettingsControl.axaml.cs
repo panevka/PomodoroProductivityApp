628571a906d09ce897f0ee25ddfa1c2871daff2a
panevka <97028404+panevka@users.noreply.github.com>
2024-12-03 11:39:49 +0100

    Creating settings panel

---
+++ PomodoroApp/SettingsControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PomodoroApp;
+
+public partial class SettingsControl : UserControl
+{
+    public SettingsControl()
+    {
+        InitializeComponent();
+    }
+}
