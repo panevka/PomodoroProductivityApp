2c1914edc0e2315628039f5b352a79c04aa383b7
panevka <97028404+panevka@users.noreply.github.com>
2024-12-04 17:56:58 +0100

    Add config saving functionality to settings button

--- PomodoroApp/SettingsControl.axaml.cs
+++ PomodoroApp/SettingsControl.axaml.cs
@@ -1,13 +1,63 @@
+using System;
+using System.Diagnostics;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace PomodoroApp;
 
 public partial class SettingsControl : UserControl
 {
+    private int _workSessionDuration;
+    private int _shortBreakDuration;
+    private int _longBreakDuration;
+
+    public int WorkSessionDuration
+    {
+        get => _workSessionDuration;
+        private set => _workSessionDuration = value;
+    }
+
+    public int ShortBreakDuration
+    {
+        get => _shortBreakDuration;
+        private set => _shortBreakDuration = value;
+    }
+    
+    public int LongBreakDuration
+    {
+        get => _longBreakDuration;
+        private set => _longBreakDuration = value;
+    }
+    
     public SettingsControl()
     {
         InitializeComponent();
+        
+        SaveButton.Click += SaveSettingsButton_Click;
+
+        WorkSessionSlider.ValueChanged += (sender, args) =>
+        {
+            WorkSessionDuration = (int)args.NewValue;
+        };
+        
+        ShortBreakSlider.ValueChanged += (sender, args) =>
+        {
+            ShortBreakDuration = (int)args.NewValue;
+        };
+
+        LongBreakSlider.ValueChanged += (sender, args) =>
+        {
+            LongBreakDuration = (int)args.NewValue;
+        };
     }
+
+    private void SaveSettingsButton_Click(object? sender, RoutedEventArgs e)
+    {
+        SettingsManager.SaveSettings(new AppSettings(WorkSessionDuration, ShortBreakDuration, LongBreakDuration));
+    }
+    
+    
 }
