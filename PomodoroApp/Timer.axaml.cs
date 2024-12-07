26dc7fd77848093e9ca65fa1a48de2f3ad1e478a
panevka <97028404+panevka@users.noreply.github.com>
2024-12-07 12:41:43 +0100

    Create TimerManager class and implement it in timer control

--- PomodoroApp/Timer.axaml.cs
+++ PomodoroApp/Timer.axaml.cs
@@ -1,41 +1,29 @@
-using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 
 namespace PomodoroApp;
 
 public partial class Timer : UserControl
-{ 
+{
+    private TimerManager _timer;
     public Timer()
     {
         InitializeComponent();
-        StartTimerCountDown(Time, SettingsManager.LoadSettings().WorkSessionDuration*60);
-
-    }
-
-    private async void StartTimerCountDown(TextBlock timeComponent, int time)
-    {
-        await UpdateTimer(timeComponent, time);
-    }
-    
-    private static async Task UpdateTimer(TextBlock timeComponent, int baseTime)
-    {
-        int seconds, hours, minutes;
-        
-        while (baseTime != 0)
+        _timer = new TimerManager(Time, SettingsManager.LoadSettings().WorkSessionDuration * 60);
+        TimerButton.Click += (sender, args) =>
         {
-            baseTime--;
-            seconds = baseTime % 60;
-            hours = Convert.ToInt32(baseTime / 60 / 60);
-            minutes = (baseTime - baseTime % 60) / 60 - hours * 60;
-            timeComponent.Text = $"{hours.ToString("00")}:{minutes.ToString("00")}:{seconds.ToString("00")}";
-            await Task.Delay(1000);
-        }
+            if (_timer.IsRunning)
+            {
+                TimerButton.Classes.Remove("PauseButton");
+                TimerButton.Classes.Add("StartButton");
+                _timer.StopTimer();
+            }
+            else
+            {
+                TimerButton.Classes.Remove("StartButton");
+                TimerButton.Classes.Add("PauseButton");
+                _timer.StartTimer(); 
+            }
+        };
+
     }
-    
-    
-    
 }
---
+++ PomodoroApp/TimerManager.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+
+namespace PomodoroApp;
+
+public class TimerManager
+{
+    private readonly TextBlock _timerTextBlock;
+    private int _currentTime;
+    private bool _isPaused = true;
+    private readonly object _lock = new();
+    private CancellationTokenSource _cancellationTokenSource;
+    
+    public TimerManager(TextBlock timerTextBlock, int timerBaseTime)
+    {
+        _currentTime = timerBaseTime;
+        _timerTextBlock = timerTextBlock;
+        UpdateTimerDisplay();
+    }
+    
+    public bool IsPaused => _isPaused;
+    public bool IsRunning => !_isPaused;
+
+    public async void StartTimer()
+    {
+        lock (_lock)
+        {
+            if (!_isPaused) return; 
+            _isPaused = false;
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        try
+        {
+            await TimerCountdownTask(_cancellationTokenSource.Token);
+        }
+        catch (OperationCanceledException)
+        {
+            lock (_lock)
+            {
+                _isPaused = true;
+                _cancellationTokenSource = null;
+            }
+        }
+    }
+
+    public void StopTimer()
+    {
+        lock (_lock)
+        {
+            if (_isPaused) return;
+            _isPaused = true;
+            _cancellationTokenSource?.Cancel();
+        }
+    }
+
+    private async Task TimerCountdownTask(CancellationToken cancellationToken)
+    {
+        while (_currentTime > 0)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            await Task.Delay(1000, cancellationToken);
+            Interlocked.Decrement(ref _currentTime);
+            UpdateTimerDisplay();
+        }
+        lock (_lock)
+        {
+            _isPaused = true;
+        }
+    }
+
+    private void UpdateTimerDisplay()
+    {
+        int seconds = _currentTime % 60;
+        int hours = Convert.ToInt32(_currentTime / 60 / 60);
+        int minutes = (_currentTime - _currentTime % 60) / 60 - hours * 60;
+        _timerTextBlock.Text = $"{hours.ToString("00")}:{minutes.ToString("00")}:{seconds.ToString("00")}";
+    }
+}
