fa553ba82dc59f5b24790debf1682e5bc618d958
panevka <97028404+panevka@users.noreply.github.com>
2024-12-10 23:31:30 +0100

    Create basic methods for switching session type and getting current session

--- PomodoroApp/Services/SessionManager.cs
+++ PomodoroApp/Services/SessionManager.cs
@@ -1,6 +1,31 @@
+using System;
+
 namespace PomodoroApp;
 
 public class SessionManager
 {
-    
+    private SessionType _currentSession;
+    private TimerManager _timer;
+    public void SwitchSessionType(TimerManager timerManager, SessionType newSessionType)
+    {
+        switch (_currentSession)
+        {
+            case SessionType.Work:
+                timerManager.ChangeTimerInitialTime(SettingsManager.LoadSettings().WorkSessionDuration);
+                break;
+            case SessionType.ShortBreak:
+                timerManager.ChangeTimerInitialTime(SettingsManager.LoadSettings().ShortBreakDuration);
+                break;
+            case SessionType.LongBreak:
+                timerManager.ChangeTimerInitialTime(SettingsManager.LoadSettings().LongBreakDuration);
+                break;
+        }
+
+        _currentSession = newSessionType;
+    }
+
+    public SessionType GetCurrentSession()
+    {
+        return _currentSession;
+    }
 }
