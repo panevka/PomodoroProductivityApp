986f9abc1af7c8b39326b1688f89b95a146e8515
panevka <97028404+panevka@users.noreply.github.com>
2024-12-12 15:03:36 +0100

    Upgrade SessionManager service: create method for auto-switching pomodoro sessions, create SessionSwitched event

--- PomodoroApp/Services/SessionManager.cs
+++ PomodoroApp/Services/SessionManager.cs
@@ -6,25 +6,62 @@ public class SessionManager
 {
     private SessionType _currentSession;
     private TimerManager _timer;
-    public void SwitchSessionType(TimerManager timerManager, SessionType newSessionType)
+    private int _cycleCounter;
+
+    public SessionManager(TimerManager timer)
     {
-        switch (_currentSession)
+        _timer = timer;
+        SwitchSessionType(SessionType.Work);
+        _timer.TimerFinished += PomodoroSessionSwitch;
+    }
+        
+    public event EventHandler SessionSwitched;
+
+    public void PomodoroSessionSwitch(object? obj, EventArgs e)
+    {
+        if (_cycleCounter == 4)
+        {
+            SwitchSessionType(SessionType.LongBreak);
+            _cycleCounter = 0;
+        }
+        else
+        {
+            switch (_currentSession)
+            {
+                case SessionType.Work:
+                    SwitchSessionType(SessionType.ShortBreak);
+                    break;
+                case SessionType.ShortBreak:
+                    SwitchSessionType(SessionType.Work);
+                    _cycleCounter++;
+                    break;
+                case SessionType.LongBreak:
+                    SwitchSessionType(SessionType.Work);
+                    break;
+            }
+        }
+        _timer.StartTimer();
+    }
+    
+    private void SwitchSessionType(SessionType newSessionType)
+    {
+        switch (newSessionType)
         {
             case SessionType.Work:
-                timerManager.ChangeTimerInitialTime(SettingsManager.LoadSettings().WorkSessionDuration);
+                _timer.ChangeTimerInitialTime(SettingsManager.LoadSettings().WorkSessionDuration*60);
                 break;
             case SessionType.ShortBreak:
-                timerManager.ChangeTimerInitialTime(SettingsManager.LoadSettings().ShortBreakDuration);
+                _timer.ChangeTimerInitialTime(SettingsManager.LoadSettings().ShortBreakDuration*60);
                 break;
             case SessionType.LongBreak:
-                timerManager.ChangeTimerInitialTime(SettingsManager.LoadSettings().LongBreakDuration);
+                _timer.ChangeTimerInitialTime(SettingsManager.LoadSettings().LongBreakDuration*60);
                 break;
         }
-
         _currentSession = newSessionType;
+        SessionSwitched?.Invoke(this, EventArgs.Empty);
     }
 
-    public SessionType GetCurrentSession()
+    public SessionType GetCurrentSessionType()
     {
         return _currentSession;
     }
