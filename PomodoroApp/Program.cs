4680c5c69d4e3142604400f9e17dc870834cde40
panevka <97028404+panevka@users.noreply.github.com>
2024-12-12 20:45:36 +0100

    Add Avalonia.Svg.Skia package for managing svg files

--- PomodoroApp/Program.cs
+++ PomodoroApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using Avalonia.Svg.Skia;
 
 namespace PomodoroApp;
 
@@ -14,8 +15,12 @@ class Program
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        GC.KeepAlive(typeof(SvgImageExtension).Assembly);
+        GC.KeepAlive(typeof(Avalonia.Svg.Skia.Svg).Assembly);
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
+    }
 }
